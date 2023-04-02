1e09c06eedcc4df0dd6bbfdc02e4cc47f6e3cf2e
DavidCarvalhido <davidcarvalhido@hotmail.com>
2023-04-02 15:08:09 +0100

    implemented reused widgets for the main screen buttons

--- lib/components/button_style.dart
+++ lib/components/button_style.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
 
-class ButtonStyles extends StatefulWidget {
-  @override
-  _ButtonStylesState createState() => _ButtonStylesState();
-}
+class ButtonListStyle extends StatelessWidget {
+  final Color? textColor;
+  final Color? backgroundColor;
+  final Size? fontSize;
+  final String label;
+  final IconData icon;
+  final VoidCallback onPressed;
+  final double? size;
 
-class _ButtonStylesState extends State<ButtonStyles> {
+  ButtonListStyle(
+      {Key? key,
+      this.textColor,
+      this.backgroundColor,
+      this.fontSize,
+      required this.label,
+      required this.icon,
+      required this.onPressed,
+      this.size})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ElevatedButton.icon(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(double.infinity, 50),
+        backgroundColor: Colors.purple,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+      ),
+      icon: Icon(
+        Icons.arrow_forward_ios,
+        size: 32,
+        color: Colors.white, // this way change the icon color
+      ),
+      label: Text(
+        label,
+        style: TextStyle(
+          color: textColor,
+          fontSize: 18.0,
+        ),
+      ),
+    );
   }
 }
--- lib/views/listViewDetail.dart
+++ lib/views/listViewDetail.dart
@@ -3,7 +3,7 @@ import 'package:two_measures/views/length.dart';
 import 'package:two_measures/views/temperature.dart';
 import 'package:two_measures/views/volume.dart';
 import 'package:two_measures/views/weight.dart';
-import 'energy.dart';
+import 'package:two_measures/views/energy.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:two_measures/components/button_style.dart';
 
@@ -16,104 +16,84 @@ class ListViewDetails extends StatelessWidget {
       padding: const EdgeInsets.only(top: 150, left: 10, right: 10),
       child: Column(
         children: [
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(double.infinity, 50),
-            ),
+          ButtonListStyle(
+            textColor: Colors.white, // deve ser para remover
+            backgroundColor: Colors.blue, // deve ser para remover
+            fontSize: Size.square(2), // deve ser para remover
+            label: AppLocalizations.of(context)!.textEnergy,
+            icon: Icons.arrow_forward_ios, // deve ser para remover
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Energy()),
+                MaterialPageRoute(
+                  builder: (context) => Energy(),
+                ),
               );
             },
-            label: Text(
-              AppLocalizations.of(context)!.textEnergy,
-            ),
-            icon: Icon(
-              Icons.arrow_forward_ios,
-              size: 32,
-              color: Colors.white, //this way change the icon color
-            ),
           ),
           spc,
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(double.infinity, 50),
-            ),
-            icon: Icon(
-              Icons.arrow_forward_ios,
-              size: 32,
-              color: Colors.white, // this way change the icon color
-            ),
+          ButtonListStyle(
+            textColor: Colors.white, // deve ser para remover
+            backgroundColor: Colors.blue, // deve ser para remover
+            fontSize: Size.square(2), // deve ser para remover
+            label: AppLocalizations.of(context)!.textLength,
+            icon: Icons.arrow_forward_ios, // deve ser para remover
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Length()),
+                MaterialPageRoute(
+                  builder: (context) => Length(),
+                ),
               );
             },
-            label: Text(
-              AppLocalizations.of(context)!.textLength,
-            ),
           ),
           spc,
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(double.infinity, 50),
-            ),
-            icon: Icon(
-              Icons.arrow_forward_ios,
-              size: 32,
-              color: Colors.white, // this way change the icon color
-            ),
+          ButtonListStyle(
+            textColor: Colors.white, // deve ser para remover
+            backgroundColor: Colors.blue, // deve ser para remover
+            fontSize: Size.square(2), // deve ser para remover
+            label: AppLocalizations.of(context)!.textTemperature,
+            icon: Icons.arrow_forward_ios, // deve ser para remover
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Temperature()),
+                MaterialPageRoute(
+                  builder: (context) => Temperature(),
+                ),
               );
             },
-            label: Text(
-              AppLocalizations.of(context)!.textTemperature,
-            ),
           ),
           spc,
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(double.infinity, 50),
-            ),
-            icon: Icon(
-              Icons.arrow_forward_ios,
-              size: 32,
-              color: Colors.white, // this way change the icon color
-            ),
+          ButtonListStyle(
+            textColor: Colors.white, // deve ser para remover
+            backgroundColor: Colors.blue, // deve ser para remover
+            fontSize: Size.square(2), // deve ser para remover
+            label: AppLocalizations.of(context)!.textVolume,
+            icon: Icons.arrow_forward_ios, // deve ser para remover
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Volume()),
+                MaterialPageRoute(
+                  builder: (context) => Volume(),
+                ),
               );
             },
-            label: Text(
-              AppLocalizations.of(context)!.textVolume,
-            ),
           ),
           spc,
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(double.infinity, 50),
-            ),
-            icon: Icon(
-              Icons.arrow_forward_ios,
-              size: 32,
-              color: Colors.white, // this way change the icon color
-            ),
+          ButtonListStyle(
+            textColor: Colors.white, // deve ser para remover
+            backgroundColor: Colors.blue, // deve ser para remover
+            fontSize: Size.square(2), // deve ser para remover
+            label: AppLocalizations.of(context)!.textWeight,
+            icon: Icons.arrow_forward_ios, // deve ser para remover
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Weight()),
+                MaterialPageRoute(
+                  builder: (context) => Weight(),
+                ),
               );
             },
-            label: Text(
-              AppLocalizations.of(context)!.textWeight,
-            ),
           ),
         ],
       ),
